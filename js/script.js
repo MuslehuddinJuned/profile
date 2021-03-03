7e3ff2fd9493d709d405ea21d4e0123dfc6910d2
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2021-03-03 19:28:34 +0600

    change theme

---
+++ js/script.js
@@ -0,0 +1,210 @@
+(function($) {
+	
+	"use strict";
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(200).fadeOut(500);
+		}
+	}
+	
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.main-header');
+			var scrollLink = $('.scroll-top');
+			if (windowpos >= 110) {
+				siteHeader.addClass('fixed-header');
+				scrollLink.fadeIn(300);
+			} else {
+				siteHeader.removeClass('fixed-header');
+				scrollLink.fadeOut(300);
+			}
+		}
+	}
+	
+	headerStyle();
+
+
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 1000);
+	
+		});
+	}
+
+
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW({
+		mobile:       false
+		});
+		wow.init();
+	}
+
+
+	//Add One Page nav
+	if($('.scroll-nav').length) {
+		$('.scroll-nav').onePageNav();
+	}
+
+
+
+
+	function galleryMasonaryLayout() {
+	    if ($('.masonary-layout').length) {
+	        $('.masonary-layout').isotope({
+	            layoutMode: 'masonry'
+	        });
+	    }
+
+	    if ($('.post-filter').length) {
+	        $('.post-filter li').children('a').on('click', function() {
+	            var Self = $(this);
+	            var selector = Self.parent().attr('data-filter');
+	            $('.post-filter li').children('a').parent().removeClass('active');
+	            Self.parent().addClass('active');
+
+
+	            $('.filter-layout').isotope({
+	                filter: selector,
+	                animationOptions: {
+	                    duration: 500,
+	                    easing: 'linear',
+	                    queue: false
+	                }
+	            });
+	            return false;
+	        });
+	    }
+
+	    if ($('.post-filter.has-dynamic-filter-counter').length) {
+	        // var allItem = $('.single-filter-item').length;
+
+	        var activeFilterItem = $('.post-filter.has-dynamic-filter-counter').find('li');
+
+	        activeFilterItem.each(function() {
+	            var filterElement = $(this).data('filter');
+	            console.log(filterElement);
+	            var count = $('.gallery-content').find(filterElement).length;
+
+	            $(this).children('span').append('<span class="count"><b>' + count + '</b></span>');
+	        });
+	    };
+	}
+
+
+	//Contact Form Validation
+	if($('#contact-form').length){
+		$('#contact-form').validate({
+			rules: {
+				username: {
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				phone: {
+					required: true
+				},
+				subject: {
+					required: true
+				},
+				message: {
+					required: true
+				}
+			}
+		});
+	}
+
+
+	//11.progressBarConfig
+	function progressBarConfig () {
+	  	var progressBar = $('.progress');
+	  		if(progressBar.length) {
+	    		progressBar.each(function () {
+	      		var Self = $(this);
+	      		Self.appear(function () {
+	        	var progressValue = Self.data('value');
+
+	        	Self.find('.progress-bar').animate({
+	          	width:progressValue+'%'           
+	        	}, 100);
+
+	        	Self.find('span.value').countTo({
+	          		from: 0,
+	            	to: progressValue,
+	            	speed: 100
+	        		});
+	     	 	});
+	   		})
+	  	}
+	}
+
+
+	function tm_animate_text(){
+	
+		"use strict";
+		
+		var animateSpan			= jQuery('.animation_text_word');
+		
+			animateSpan.typed({
+				strings: ["User Interface Designer", "Creative Web Developer", "User Interface Designer"],
+				loop: true,
+				startDelay: 1e3,
+				backDelay: 3e3
+			});
+	}
+
+	jQuery(document).on('ready', function () {
+		(function ($) {
+			tm_animate_text();
+		})(jQuery);
+	});
+
+
+	/*	=========================================================================
+	When document is Scrollig, do
+	========================================================================== */
+
+	jQuery(document).on('ready', function () {
+		(function ($) {
+			// add your functions
+			progressBarConfig ();
+			tm_animate_text();
+		})(jQuery);
+	});
+
+
+
+	/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+
+	
+	
+	/* ==========================================================================
+   When document is loaded, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+		galleryMasonaryLayout()
+	});
+
+	
+
+})(window.jQuery);
